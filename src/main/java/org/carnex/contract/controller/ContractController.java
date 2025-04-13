c9823a62be7b22e0be957d7bc8766364419a7594
KONGEUNAI <kea4150@gmail.com>
2025-04-13 16:23:34 +0900

    Update ContractController.java

--- src/main/java/org/carnex/contract/controller/ContractController.java
+++ src/main/java/org/carnex/contract/controller/ContractController.java
@@ -40,50 +40,23 @@ public class ContractController {
 	
 	@GetMapping("/contract")
 	public void contract(@RequestParam("car_num") int car_num, Model model) throws Exception {
-		// 차 하나 받아와서 자동 입력
+
 		GoodsVO gvo = goodsService.getCarOne(car_num);
 		model.addAttribute("gvo", gvo);
-		
-		// 유저 세션이 있으면 구매자 정보 자동 입력 (온라인 고객)
-		// 오프라인 고객 = 직접 입력
-//		if (session.getAttribute("session") != null) {
-//			MemberVO mvo = (MemberVO) session.getAttribute("session");
-//			model.addAttribute("mvo", mvo);
-//		}
-		
-		// 카드 번호 받아서 카드정보가 있으면 카드 정보 자동 입력
-		// 없으면 0 빈값을 대입
-//		CardVO cardvo = (CardVO) userMemberService.getCard(card_num);
-//		if (cardvo != null) {
-//			model.addAttribute("cardvo", cardvo);
-//		} else {
-//			CardVO excardvo = new CardVO();
-//			excardvo.setCard_num(0);
-//			model.addAttribute("cardvo", excardvo);
-//		}
-		
-		// 현재 날짜 가져오기
-        LocalDate today = LocalDate.now();
 
-        // 날짜 형식 지정
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        	LocalDate today = LocalDate.now();
 
-        // 형식에 맞게 변환
-        String date = today.format(formatter);
-        model.addAttribute("date", date);
+        	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+	        String date = today.format(formatter);
+	        model.addAttribute("date", date);
 	}
 	
 	@PostMapping("/contract")
 	public String contract(HttpServletRequest request, GoodsVO gvo, MemberVO mvo, CardVO cvo) throws Exception {
-//		int card_num = Integer.parseInt(request.getParameter("card_num"));
+
 		String sign = request.getParameter("con_sign");
-//		if (card_num != 0) {
-//			// card_num으로 카드 찾기
-//			CardVO cardvo = (CardVO) userMemberService.getCard(card_num);
-//			// card_num 있을때 cardvo는 따로 service 태워서 insert 
-//			contractService.insertCard(cardvo);
-//		}
-		// cardVO 제외 gvo mvo 서명 데이터 매퍼로 전달
+
 		contractService.writeContract(gvo, mvo, cvo, sign);
 		String staff_id = gvo.getStaff_id();
 		int con_no = contractService.getCount(staff_id);
@@ -96,11 +69,4 @@ public class ContractController {
 		List<ContractVO> conList = contractService. getContractList();
 		model.addAttribute("cvo", conList);
 	}
-	
-//	@GetMapping("/delete")
-//	public void delete(@RequestParam("car_num") int car_num) throws Exception {
-//		ContractVO convo = contractService.deleteContract(car_num);
-//		
-//	}
-
 }
